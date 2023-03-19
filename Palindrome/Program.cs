7bd09b0e96805d6b55b71358bcfeb9625ea589d2
g3orgios <gnik88@gmail.com>
2023-03-19 16:01:32 +0200

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Console.WriteLine("Enter a list of words separated by commas:");
+string input = Console.ReadLine();
+string[] words = input.Split(',');
+foreach (string word in words)
+{
+    if (IsPalindrome(word))
+    {
+        Console.WriteLine(word);
+    }
+}
+
+bool IsPalindrome(string word)
+{
+    for (int i = 0; i < word.Length / 2; i++)
+    {
+        if (word[i] != word[word.Length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
