d2c2ee487adf76b40a4513179b9528913256adf2
Giorgos Nikolopoulos <120304392+g3orgios@users.noreply.github.com>
2023-03-19 16:34:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a list of words (separated by commas):");
+        string input = Console.ReadLine();
+
+        List<string> words = new List<string>(input.Split(','));
+
+        List<string> wordsWithO = new List<string>();
+
+        foreach (string word in words)
+        {
+            if (word.Contains("o"))
+            {
+                wordsWithO.Add(word);
+            }
+        }
+
+        Console.WriteLine("Words containing 'o':");
+        foreach (string word in wordsWithO)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
+
